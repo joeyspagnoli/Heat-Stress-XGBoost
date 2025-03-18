d0cd402fa25648c2732e9d0c96cf4795efbfb8f1
Joey Spagnoli <joseph.spagnoli@ufl.edu>
2025-03-18 13:12:27 -0400

    More weather data

--- file_preperation.py
+++ file_preperation.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 def remove_rows():
-    df = pd.read_csv("California Weather 08.csv")
+    df = pd.read_csv("Raw Weather/Oklahoma Weather 08.csv")
     print(df.head())
     df.dropna(axis=0, how='any', subset=['TAVG', 'TMAX', 'TMIN'], inplace=True)
     # df.drop(columns=['STATION', 'NAME', 'DAEV','DAPR','EVAP',	'MDEV',	'MDPR',	'MNPN',	'MXPN',	'PRCP',
     #                       	'SNOW',	'SNWD', 'TOBS'], inplace=True)
-    df.drop(columns=['STATION', 'NAME', 'LATITUDE', 'LONGITUDE', 'EVAP', 'ELEVATION', 'PRCP', 'TOBS'], inplace=True)
+    df.drop(columns=['STATION', 'NAME', 'TOBS'], inplace=True)
 
-    df.to_csv('CA08Updated.csv', index=False, na_rep='NA')
+    df.to_csv('Cleaned Weather/OK08Updated.csv', index=False, na_rep='NA')
 
 remove_rows()
